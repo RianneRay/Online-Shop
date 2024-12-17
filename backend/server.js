10c9611b56805bdf01d0a9bff1d26f4a8a587750
Rianne Ray Relevo <riannerayrelevo04.com>
2024-12-17 15:34:48 +0800

    Added controllers and routes; updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,12 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js'
+import productRoutes from './routes/product.route.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/api/products", async (req, res) => {
-  const product = req.body;
-  
-  if(!product.name || !product.price || !product.image){
-    return res.status(400).json({success: false, message: "please provide all fields" });
-  }
-  
-  const newProduct = new Product(product)
-  ;
-  try {
-    await newProduct.save();
-    res.status(201).json({success: true, data: newProduct});
-  } catch (error) {
-    console.error("Error in creating product: ", error.message);
-    res.status(500).json({success: false, message: "Server Error"});
-  }
-});
+app.use('/api/products', productRoutes);
 
 app.listen(4000, () => {
   connectDB();
