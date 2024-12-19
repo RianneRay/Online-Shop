aea468b9f33735b804d063ba41c9c75ab7f66a67
Rianne Ray Relevo <riannerayrelevo04.com>
2024-12-19 15:05:34 +0800

    Finish project and prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 const PORT = process.env.PORT || 4000
 
+const __dirname = path.resolve();
+
 app.use('/api/products', productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,37 +1,57 @@
-import { Container, VStack, Text, SimpleGrid } from '@chakra-ui/react';
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { useProductStore } from '../store/product.js'
+import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product";
+import ProductCard from "../components/ProductCard";
 
 const HomePage = () => {
-  const {fetchProducts, products} = useProductStore();
-  useEffect(() => {
-    fetchProducts();
-  }, [fetchProducts])
-  console.log("Products", products);
-  
-  return (
-    <Container maxW="container.xl" py={12}>
-      <VStack spacing={8}>
-        <Text
+	const { fetchProducts, products } = useProductStore();
+
+	useEffect(() => {
+		fetchProducts();
+	}, [fetchProducts]);
+	console.log("products", products);
+
+	return (
+		<Container maxW='container.xl' py={12}>
+			<VStack spacing={8}>
+				<Text
           fontSize={{ base: "22", sm: "28" }}
           fontWeight={"bold"}
+          textTransform={"uppercase"}
           textAlign={"center"}
           bgGradient={"linear(to-r, #D1C6B1, #A68F63)"}
           bgClip={"text"}
-        > Explore Our Collection
+        >
+					Uncover Amazing Products at Unbeatable Prices
 				</Text>
-				
-				<Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
-					No products to display{" "}
-					<Link to={"/create"}>
-						<Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
-							Create a product
-						</Text>
-					</Link>
-				</Text>
-      </VStack>
-    </Container>);
-};
 
+				<SimpleGrid
+					columns={{
+						base: 1,
+						md: 2,
+						lg: 3,
+					}}
+					spacing={10}
+					w={"full"}
+				>
+					{products.map((product) => (
+						<ProductCard key={product._id} product={product} />
+					))}
+				</SimpleGrid>
+
+				{products.length === 0 && (
+					<Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
+						No products found 😢{" "}
+						<Link to={"/create"}>
+							<Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
+								Create a product
+							</Text>
+						</Link>
+					</Text>
+				)}
+			</VStack>
+		</Container>
+	);
+};
 export default HomePage;
